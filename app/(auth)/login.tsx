642db042e031d730b134ec7c8af8f40d755e74d6
Piyush289kumar <piyushraikwar289@gmail.com>
2025-11-30 17:28:12 +0530

    Create Login Page.

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,1 +1,12 @@
 // app/(auth)/ login.tsx
+
+import { Text } from "@/components/ui/text";
+import { View } from "@/components/ui/view";
+
+export default function LoginPage(){
+    return(
+        <View>
+            <Text>Login Page</Text>
+        </View>
+    )
+}
