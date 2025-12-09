fdca265eb38731959d485ed151af638d61a6ded4
Piyush289kumar <piyushraikwar289@gmail.com>
2025-12-10 01:09:40 +0530

    Working on Profile Page.

--- app/(tabs)/(profile)/index.tsx
+++ app/(tabs)/(profile)/index.tsx
@@ -1,7 +1,9 @@
 // app/(tabs)/(profile)/index.tsx
 
+import { IconDemo } from "@/components/demo/icon/icon";
 import { SearchBarSuggestions } from "@/components/search/searchbar";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import { Icon } from "@/components/ui/icon";
 import { ModeToggle } from "@/components/ui/mode-toggle";
@@ -9,8 +11,8 @@ import { ScrollView } from "@/components/ui/scroll-view";
 import { Text } from "@/components/ui/text";
 import { View } from "@/components/ui/view";
 import { useColor } from "@/hooks/useColor";
-import { PRIMARY_COLOR } from "@/theme/globals";
-import { Code, Eye, Palette, Settings } from "lucide-react-native";
+import { BORDER_RADIUS, CORNERS, PADDING_HORIZONTAL, PRIMARY_COLOR } from "@/theme/globals";
+import { Code, Eye, Palette, QrCode, Settings } from "lucide-react-native";
 
 export default function ProfileScreen() {
   const card = useColor("card");
@@ -62,9 +64,12 @@ export default function ProfileScreen() {
               </Avatar>
 
                <View 
-            style={{ flexDirection: 'column', alignItems: 'center', gap: 10 }}>
+            style={{ flexDirection: 'column', alignItems: 'flex-start', gap: 10 }}>
               <Text variant="subtitle">Piyush Kumar Raikwar</Text>
-              <Text variant="subtitle">Piyush Kumar Raikwar</Text>
+                <Badge>★ Elite Plan</Badge>
+            </View>
+            <View style={{backgroundColor:"#000", borderRadius: CORNERS, padding:6}}>
+            <Icon name={QrCode} size={32}/>
             </View>
             </View>
           </CardContent>
