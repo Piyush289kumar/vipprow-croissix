8d9a3f2d000144c67d0241f6c60432f808b901b5
Piyush289kumar <piyushraikwar289@gmail.com>
2025-12-11 23:09:44 +0530

    Remove QR.

--- app/(tabs)/(profile)/index.tsx
+++ app/(tabs)/(profile)/index.tsx
@@ -1,6 +1,5 @@
 // app/(tabs)/(profile)/index.tsx
 
-import { IconDemo } from "@/components/demo/icon/icon";
 import { SearchBarSuggestions } from "@/components/search/searchbar";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
@@ -11,7 +10,12 @@ import { ScrollView } from "@/components/ui/scroll-view";
 import { Text } from "@/components/ui/text";
 import { View } from "@/components/ui/view";
 import { useColor } from "@/hooks/useColor";
-import { BORDER_RADIUS, CORNERS, PADDING_HORIZONTAL, PRIMARY_COLOR } from "@/theme/globals";
+import {
+  BORDER_RADIUS,
+  CORNERS,
+  PADDING_HORIZONTAL,
+  PRIMARY_COLOR,
+} from "@/theme/globals";
 import { Code, Eye, Palette, QrCode, Settings } from "lucide-react-native";
 
 export default function ProfileScreen() {
@@ -39,10 +43,12 @@ export default function ProfileScreen() {
           marginBottom: 40,
         }}
       >
+        {/* Profile Section Start */}
         <Card>
           <CardContent>
-            <View 
-            style={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 16 }}
+            >
               <Avatar
                 size={64}
                 style={{
@@ -63,17 +69,20 @@ export default function ProfileScreen() {
                 <AvatarFallback>AB</AvatarFallback>
               </Avatar>
 
-               <View 
-            style={{ flexDirection: 'column', alignItems: 'flex-start', gap: 10 }}>
-              <Text variant="subtitle">Piyush Kumar Raikwar</Text>
+              <View
+                style={{
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  gap: 10,
+                }}
+              >
+                <Text variant="subtitle">Piyush Kumar Raikwar</Text>
                 <Badge>★ Elite Plan</Badge>
-            </View>
-            <View style={{backgroundColor:"#000", borderRadius: CORNERS, padding:6}}>
-            <Icon name={QrCode} size={32}/>
-            </View>
+              </View>
             </View>
           </CardContent>
         </Card>
+        {/* Profile Section End */}
       </View>
     </ScrollView>
   );
